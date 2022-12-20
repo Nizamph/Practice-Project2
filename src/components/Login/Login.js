7f72e6ac36b2bc2cebeb56d1c67e9b52407f0f82
Nizamph <116118003+Nizamph@users.noreply.github.com>
2022-12-21 00:37:06 +0530

    created seperate context management Component(Context Provider)

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
   if(action.type === 'USER_INPUT') {
@@ -37,6 +38,8 @@ const Login = (props) => {
   
   const [emailState, dispatchEmail] =  useReducer(emailReducer,{value: '', isValid: null,})
   const [passwordState, dispatchPassword] = useReducer(passwordReducer,{value:'', isValid: null,})
+
+  const authCtx = useContext(AuthContext)
 
   useEffect(() => {
     console.log("use effect running")//only run when this component function is executed initially and unmounted from the DOM(which menas deleted from the screen(once we logged in))
@@ -109,7 +112,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value, enteredCollegeName);
+    authCtx.onLogin(emailState.value, passwordState.value, enteredCollegeName);
   };
 
   return (
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,37 @@
-import react from "react"
+import react,{ useState,useEffect } from "react";
 
 const AuthContext = react.createContext({
-    isLoggedIn: false
+    isLoggedIn: false,
+    onLogout: () => {},//just for accessing with IDE nothing much
+    onLogin: (email, password) => {} //just for accessing with IDE nothing much
 })
 
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn')
+    
+        if(storedUserLoggedInInformation === '1') {
+          setIsLoggedIn(true);
+        }
+      }, []);
+
+    const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn')
+        setIsLoggedIn(false);
+    }
+
+    const loginHandler = () => {
+        localStorage.setItem('isLoggedIn', '1')
+        setIsLoggedIn(true)
+    }
+    return <AuthContext.Provider value={{
+        isLoggedIn: isLoggedIn, 
+        onLogout: logoutHandler, 
+        onLogin: loginHandler}}
+        >
+            {props.children}
+            </AuthContext.Provider>
+}
 export default AuthContext
